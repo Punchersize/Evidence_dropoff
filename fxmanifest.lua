ec0e5ad5c36c677f653fd6f399cee77ea4d4617f
Punchy <142583931+Punchersize@users.noreply.github.com>
2023-08-18 20:17:28 +1000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,11 @@
 fx_version 'cerulean'
 game 'gta5'
 
--- Define the resource
-name 'trunk-gun-storage'
+name 'Evidence DropOff'
 description 'A FiveM script that allows storing guns in the trunk of a vehicle'
 author 'Punchersize'
 version '1.0.0'
 
--- Define the client script that handles the client-side logic
 client_scripts {
     "@NativeUI/NativeUI.lua",
     "client.lua"
